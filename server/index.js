641739256c63815f72b3f30d33f60b91ec5e16b1
James Proctor <59664392+atchafalaya@users.noreply.github.com>
2021-11-17 16:14:15 -0600

    add slash to pic

--- server/index.js
+++ server/index.js
@@ -5,10 +5,14 @@ const path = require("path");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html')); //when GET request, send absolute filepath, join returns path dirname is file, string is relative filepath 
+    res.sendFile(path.join(__dirname, '../public/index.html')); //when GET request, send absolute filepath, join returns path dirname is file, string is relative filepath 
 })
 
 const port = process.env.PORT || 4005;
+
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/main.js'))
+  })
 
 app.listen(port, () => {
     console.log(`App running PORT ${port}`);
