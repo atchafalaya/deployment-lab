5d1265fcabb8d6c43437762ff0c46fe9b2dc2bc3
James Proctor <59664392+atchafalaya@users.noreply.github.com>
2021-11-17 21:22:36 -0600

    changed css and dirname

--- server/index.js
+++ server/index.js
@@ -5,41 +5,41 @@ const path = require("path");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 
 }) 
 
-app.get('/styles.css', (req, res) => {
-    res.sendFile(path.join(dirname, '../public/styles.css'));
+app.get('/style.css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/style.css'));
 })
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(dirname, '../server/index.js'));
+    res.sendFile(path.join(__dirname, '../server/index.js'));
 
 });
 
 app.get('/images/Sailing.jpg', (req, res) => {
-    res.sendFile(path.join(dirname, '../images/Sailing.jpg'));
+    res.sendFile(path.join(__dirname, '../images/Sailing.jpg'));
 });
 
 app.get('/js', (req, res) => {
-    res.sendFile(path.join(dirname, '../public/main.js'))
+    res.sendFile(path.join(__dirname, '../public/main.js'))
   })
 
-  app.use('/js', express.static(path.join(dirname, '../public/main.js')));
+  app.use('/js', express.static(path.join(__dirname, '../public/main.js')));
 
-  app.use('/server/index.js', express.static(path.join(dirname, '../server/index.js')));
+  app.use('/server/index.js', express.static(path.join(__dirname, '../server/index.js')));
 
-  app.use('/images', express.static(path.join(dirname, '../images')));
+  app.use('/images', express.static(path.join(__dirname, '../images')));
 
   app.use(express.static(__dirname +'../public'));
   
-  app.get('/js', (req, res) => {
-      res.sendFile(path.join(__dirname, '../public/main.js'))
+ // app.get('/js', (req, res) => {
+ //     res.sendFile(path.join(__dirname, '../public/main.js'))
 
   const port = process.env.PORT || 4005;
 
-  })
+//  })
 
 app.listen(port, () => {
     console.log(`App running PORT ${port}`);
