0b7d3902df32d9886add2bf8f1ba93614ae3a4bb
James Proctor <59664392+atchafalaya@users.noreply.github.com>
2021-11-18 15:34:52 -0600

    cleaned up code

--- server/index.js
+++ server/index.js
@@ -13,15 +13,6 @@ app.get('/style.css', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/style.css'));
 })
 
-//app.get('/', (req,res) => {
-  //  res.sendFile(path.join(__dirname, '../server/index.js'));
-
-//});
-
-// app.get('/images/Sailing.jpg', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../images/Sailing.jpg'));
-// });
-
 app.get('/js', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/main.js'))
   })
@@ -35,13 +26,8 @@ app.get('/js', (req, res) => {
   app.use('/images', express.static(path.join(__dirname, '../images')));
 
   app.use(express.static(__dirname +'../public')); //may not need
-  
- // app.get('/js', (req, res) => {
- //     res.sendFile(path.join(__dirname, '../public/main.js'))
 
   const port = process.env.PORT || 4005;
-
-//  })
 
 app.listen(port, () => {
     console.log(`App running PORT ${port}`);
