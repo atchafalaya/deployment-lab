2b656f6fd6c8bf3a96f4b1a38b07ba1ca0af06f3
James Proctor <59664392+atchafalaya@users.noreply.github.com>
2021-11-17 20:45:05 -0600

    4th commit

--- server/index.js
+++ server/index.js
@@ -3,12 +3,38 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+app.get("/", (req, res) => {
+    res.sendFile(path.join(dirname, '../public/store.html'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html')); //when GET request, send absolute filepath, join returns path dirname is file, string is relative filepath 
+}) 
+
+app.get('/style.css', (req, res) => {
+    res.sendFile(path.join(dirname, '../public/style.css'));
 })
 
-const port = process.env.PORT || 4005;
+app.get('/', (req,res) => {
+    res.sendFile(path.join(dirname, '../server/index.js'));
+
+});
+
+app.get('/images/Sailing.jpg', (req, res) => {
+    res.sendFile(path.join(dirname, '../images/Sailing.jpg'));
+});
+
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(dirname, '../public/main.js'))
+  })
+
+  app.use('/js', express.static(path.join(dirname, '../public/main.js')));
+
+  app.use('/server/index.js', express.static(path.join(dirname, '../server/index.js')));
+
+  app.use('/images', express.static(path.join(dirname, '../images')));
+
+  app.use(express.static(__dirname +'../public'));
+
+
+  const port = process.env.PORT || 4005;
 
 app.get('/js', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/main.js'))
