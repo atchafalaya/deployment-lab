90483e76837c12e30d49651c97a4d0a82b3ac4ef
James Proctor <59664392+atchafalaya@users.noreply.github.com>
2021-11-17 20:49:05 -0600

    5th

--- server/index.js
+++ server/index.js
@@ -3,8 +3,9 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+
 app.get("/", (req, res) => {
-    res.sendFile(path.join(dirname, '../public/store.html'));
+    res.sendFile(path.join(dirname, '../public/index.html'));
 
 }) 
 
