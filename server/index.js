e05055341316cf726eaeb7af4bdc6829e1fc5e8f
James Proctor <59664392+atchafalaya@users.noreply.github.com>
2021-11-18 09:55:42 -0600

    comment out unn

--- server/index.js
+++ server/index.js
@@ -13,10 +13,10 @@ app.get('/style.css', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/style.css'));
 })
 
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '../server/index.js'));
+//app.get('/', (req,res) => {
+  //  res.sendFile(path.join(__dirname, '../server/index.js'));
 
-});
+//});
 
 // app.get('/images/Sailing.jpg', (req, res) => {
 //     res.sendFile(path.join(__dirname, '../images/Sailing.jpg'));
@@ -26,13 +26,15 @@ app.get('/js', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/main.js'))
   })
 
-  app.use('/js', express.static(path.join(__dirname, '../public/main.js')));
+  app.use('/js', express.static(path.join(__dirname, '../public/main.js'))); //frontend javascript
 
-  app.use('/server/index.js', express.static(path.join(__dirname, '../server/index.js')));
+  app.use('/style.css', express.static(path.join(__dirname, '/public/style.css')));
+
+  //app.use('/server/index.js', express.static(path.join(__dirname, '../server/index.js'))); //already called in package.json
 
   app.use('/images', express.static(path.join(__dirname, '../images')));
 
-  app.use(express.static(__dirname +'../public'));
+  app.use(express.static(__dirname +'../public')); //may not need
   
  // app.get('/js', (req, res) => {
  //     res.sendFile(path.join(__dirname, '../public/main.js'))
