7d8d8f9f253d389c5880b33ae5292ab252c7f462
James Proctor <59664392+atchafalaya@users.noreply.github.com>
2021-11-18 08:14:14 -0600

    commenting out

--- server/index.js
+++ server/index.js
@@ -4,27 +4,27 @@ const path = require("path");
 
 const app = express();
 
-//app.get("/", (req, res) => {
-//    res.sendFile(path.join(__dirname, '../public/index.html'));
+app.get("/", (req, res) => {
+   res.sendFile(path.join(__dirname, '../public/index.html'));
 
-// }) 
+}) 
 
-// app.get('/style.css', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public/style.css'));
-// })
+app.get('/style.css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/style.css'));
+})
 
-// app.get('/', (req,res) => {
-//     res.sendFile(path.join(__dirname, '../server/index.js'));
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../server/index.js'));
 
-// });
+});
 
 // app.get('/images/Sailing.jpg', (req, res) => {
 //     res.sendFile(path.join(__dirname, '../images/Sailing.jpg'));
 // });
 
-// app.get('/js', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public/main.js'))
-//   })
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/main.js'))
+  })
 
   app.use('/js', express.static(path.join(__dirname, '../public/main.js')));
 
